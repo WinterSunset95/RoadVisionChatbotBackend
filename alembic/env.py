bc4abc44e906787a1c33ffbd4182046a5a82bc5d
WinterSunset95 <wintersunset95@gmail.com>
2025-10-30 11:47:36 +0530

    feat: first alembic script generation

--- alembic/env.py
+++ alembic/env.py
@@ -12,6 +12,8 @@ from alembic import context
 from app.config import settings
 from app.db.database import Base
 from app.modules.askai.db import models
+import pgvector
+from pgvector.sqlalchemy import Vector
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
